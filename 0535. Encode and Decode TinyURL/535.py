2fcec7c16e67e6f422ad1da4c72ed40a3631a362
Aarav Shah <aaravshah0011@gmail.com>
2024-07-29 11:46:29 +0530

    Q535

---
+++ 0535. Encode and Decode TinyURL/535.py
@@ -0,0 +1,17 @@
+class Codec:
+    def __init__(self):
+        self.dict = {}
+        self.key = 0
+
+    def encode(self, longUrl: str) -> str:
+        self.dict[self.key] = longUrl
+        self.key += 1
+        return self.key-1
+
+    def decode(self, shortUrl: str) -> str:
+        return self.dict[shortUrl]
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
